95f3e66ec7025c797cd40603ef28c5c4e37e88e9
Charley <charley1135@gmail.com>
2022-10-16 12:42:50 -0700

    feat:Added In Column For URL.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
@@ -7,3 +8,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2,null=True)
     inventory_quantity = models.IntegerField(null=True)
+    picture_url = models.CharField(max_length=255,null=True)
